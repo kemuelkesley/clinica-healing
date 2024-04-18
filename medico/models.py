7104a9ebd452de0e4e0af016f96293f5fa2316a8
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2024-04-18 09:56:59 -0300

    agendamento medico concluido

--- medico/models.py
+++ medico/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime
 
 # Create your models here.
 
@@ -31,8 +31,14 @@ class DadosMedico(models.Model):
     especialidade = models.ForeignKey(Especialidades, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return self.nome
+        return self.user.username
     
+    @property
+    def proxima_data(self):
+        proxima_data = DatasAbertas.objects.filter(user=self.user).filter(data__gt=datetime.now()).filter(agendado=False).order_by('data').first()
+
+        return proxima_data
+
 
 class DatasAbertas(models.Model):
     data = models.DateTimeField()
---
+++ paciente/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from medico.models import DadosMedico
+
+# Create your views here.
+
+def home(request):
+    if request.method == "GET":
+        medicos = DadosMedico.objects.all()
+        print(medicos)
+        return render(request, 'home.html', {'medicos' : medicos})
+    
+
